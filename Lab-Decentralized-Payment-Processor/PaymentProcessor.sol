97770774ec76bf010f441616eac8f7dd14e6b406
Brayan Markov <brayan_markov@yahoo.com>
2024-12-08 18:41:40 +0200

    Add decentralized payment processor

---
+++ Lab-Decentralized-Payment-Processor/PaymentProcessor.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+contract PaymentProcessor {
+    mapping (address => uint256) public balances;
+
+    event PaymentReceived(address indexed customer, uint256 amount);
+    event RefundProcessed(address indexed customer, uint256 refundAmount);
+    event BonusAdded(address indexed customer, uint256 bonusAmount);
+
+    function receivePayment() external payable {
+        require(msg.value > 0, "Payment must be greather than 0");
+
+        balances[msg.sender] += msg.value;
+        emit PaymentReceived(msg.sender, msg.value);
+    }
+
+    function checkBalance() external view returns(uint256) {
+        return balances[msg.sender];
+    }
+
+    function refundPayment(address customer, uint256 refundAmount) public virtual {
+        require(balances[customer] >= refundAmount, "Insufficient balance");
+
+        balances[customer] -= refundAmount;
+        payable(customer).transfer(refundAmount);
+
+        emit RefundProcessed(customer, refundAmount);
+    }
+}
+
+contract Merchant is PaymentProcessor {
+    function refundPayment(address customer, uint256 refundAmount) public override {
+        require(balances[customer] >= refundAmount, "Insufficient balance");
+
+        uint256 bonus = refundAmount / 100;
+        uint totalRefund = refundAmount + bonus;
+
+        require(address(this).balance >= totalRefund, "Balance too low...");
+
+        balances[customer] -= refundAmount;
+        payable(customer).transfer(totalRefund);
+
+        emit RefundProcessed(customer, refundAmount);
+        emit BonusAdded(customer, bonus);
+    }
+}
