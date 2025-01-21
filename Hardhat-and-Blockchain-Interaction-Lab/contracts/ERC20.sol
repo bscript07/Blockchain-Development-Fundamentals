b101a136b8607b9ec2719315fe8e917101f7a73e
Brayan Markov <145554659+bscript07@users.noreply.github.com>
2025-01-21 12:40:56 +0200

    Update ERC20.sol

--- Hardhat-and-Blockchain-Interaction-Lab/contracts/ERC20.sol
+++ Hardhat-and-Blockchain-Interaction-Lab/contracts/ERC20.sol
@@ -1,1 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
 
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Dogecoin is ERC20, Ownable {
+    constructor(string memory name, string memory symbol, uint256 initialSupply) ERC20(name, symbol) Ownable(msg.sender) {
+        _mint(msg.sender, initialSupply);
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
