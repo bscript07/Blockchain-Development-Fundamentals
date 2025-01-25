1b047fc2841fb888e5f265ce8d2a25d3e8450107
Brayan Markov <145554659+bscript07@users.noreply.github.com>
2025-01-25 18:22:13 +0200

    Update 02_DisplayCurrentBlockNumber.js

--- Hardhat-and-Blockchain-Interaction-Lab/tasks/02_DisplayCurrentBlockNumber.js
+++ Hardhat-and-Blockchain-Interaction-Lab/tasks/02_DisplayCurrentBlockNumber.js
@@ -1,17 +1,12 @@
-const { task } = require("hardhat/config");
-
 task("displayBlock", "Fetches and display the current block number").setAction(
   async (_, hre) => {
-    const { ethers } = hre; // Destructure ethers from hre (Hardhat Runtime Environment)
-
     try {
-      const blockNumber = await ethers.provider.getBlockNumber();
+      const blockNumber = await hre.ethers.provider.getBlockNumber();
       console.log(`Current block number: ${blockNumber}`);
     } catch (error) {
       console.log("Error fetching the block number", error);
     }
   }
-  
 );
 
 module.exports = {};
