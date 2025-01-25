31ae01c8dd1bbfbd76c4d16e985cdb56b33a331e
Brayan Markov <145554659+bscript07@users.noreply.github.com>
2025-01-25 18:23:18 +0200

    Update 05_ETHTransfer.js

--- Hardhat-and-Blockchain-Interaction-Lab/tasks/05_ETHTransfer.js
+++ Hardhat-and-Blockchain-Interaction-Lab/tasks/05_ETHTransfer.js
@@ -1,31 +1,32 @@
-const { task } = require("hardhat/config");
-
 task("ethTransfer", "Transfer ETH between accounts")
-.addParam("recipient", "The address of the recipient")
-.addParam("amount", "The amount of ETH to transfer")
-.setAction(async({recipient, amount}) => {
-    const {ethers} = hre;
-
-    const [sender] = await ethers.getSigners();
+  .addParam("recipient", "The address of the recipient")
+  .addParam("amount", "The amount of ETH to transfer")
+  .setAction(async ({ recipient, amount }, hre) => {
+    const [sender] = await hre.ethers.getSigners();
     console.log(`Sender address: ${sender.address}`);
     console.log(`Recipient address: ${recipient}`);
     console.log(`Amount to transfer: ${amount} ETH`);
-    
+
     // Convert amount from ETH to Wei
-    const amountWei = ethers.utils.parseEther(amount);
+    const amountWei = await hre.ethers.parseEther(amount);
+    console.log(`Amount in Wei: ${amountWei.toString()}`);
 
     try {
-        const tx = await sender.sendTransaction({
-            to: recipient,
-            value: amountWei
-        });
+      const tx = await sender.sendTransaction({
+        to: recipient,
+        value: amountWei,
+      });
 
-        // Wait for the transaction to be confirmed
-        await tx.wait();
-        console.log("Transaction confirmed");
+      if (sender.address == recipient) {
+        throw new Error("Sender and recipient addresses are the same");
+      }
+
+      // Wait for the transaction to be confirmed
+      await tx.wait();
+      console.log("Transaction confirmed");
     } catch (error) {
-        console.log("Transaction failed:", error);
+      console.log("Transaction failed:", error);
     }
-});
+  });
 
 module.exports = {};
