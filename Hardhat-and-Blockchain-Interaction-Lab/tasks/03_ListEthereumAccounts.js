2248be6083ffbfadb1a6cb9fbab039929c3d3bad
Brayan Markov <145554659+bscript07@users.noreply.github.com>
2025-01-25 18:22:31 +0200

    Update 03_ListEthereumAccounts.js

--- Hardhat-and-Blockchain-Interaction-Lab/tasks/03_ListEthereumAccounts.js
+++ Hardhat-and-Blockchain-Interaction-Lab/tasks/03_ListEthereumAccounts.js
@@ -1,21 +1,19 @@
-const { task } = require("hardhat/config");
+task(
+  "retrieveAndPrintAddresses",
+  "Retrieve and print addresses of all accounts"
+).setAction(async (_, hre) => {
+  try {
+    const accounts = await hre.ethers.getSigners();
+    let counter = 0;
 
-task("retrieveAndPrintAddresses", "Retrieve and print addresses of all accounts")
-.setAction(async(_, hre) => {
-    const {ethers} = hre;
-
-    try {
-        const accounts = await ethers.getSigners();
-        let counter = 0;
-    
-        accounts.forEach((account) => {
-            console.log(`Account address: ${account.address}`);
-            console.log(counter++);
-        });   
-    } catch (error) {
-        console.log("Error retrieving accounts:", error);
-    }
-
+    accounts.forEach((account) => {
+      console.log(`Account address: ${account.address}`);
+      console.log(counter++);
+    });
+  } catch (error) {
+    console.log("Error retrieving accounts:", error);
+  }
 });
 
 module.exports = {};
+
