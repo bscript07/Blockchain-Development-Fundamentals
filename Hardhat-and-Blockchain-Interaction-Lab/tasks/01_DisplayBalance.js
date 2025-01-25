e651456a7fa9a7a1ee4979c0c8497d6f1d89996a
Brayan Markov <145554659+bscript07@users.noreply.github.com>
2025-01-25 18:21:52 +0200

    Update 01_DisplayBalance.js

--- Hardhat-and-Blockchain-Interaction-Lab/tasks/01_DisplayBalance.js
+++ Hardhat-and-Blockchain-Interaction-Lab/tasks/01_DisplayBalance.js
@@ -1,20 +1,17 @@
-const { task } = require("hardhat/config");
-
 task(
   "displayBalance",
   "Display the ethers balance of the first account"
 ).setAction(async (_, hre) => {
-  const { ethers } = hre; // Destructure ethers from hre (Hardhat Runtime Environment)
-
   try {
-    const [firstAccount] = await ethers.getSigners();
-    const balance = await ethers.provider.getBalance(firstAccount.address);
-    const formattedBalance = hre.ethers.utils.formatEther(balance);
+    const [firstAccount] = await hre.ethers.getSigners();
+    const balance = await hre.ethers.provider.getBalance(firstAccount.address);
+
+    const formattedBalance = hre.ethers.formatEther(balance);
     console.log(`Balance task loaded successfully ${formattedBalance}`);
   } catch (error) {
     console.log("Error fetching the block number", error);
   }
-
 });
 
 module.exports = {};
+
