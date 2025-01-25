95fb498176d5108c1006d6eb0d760645543545ae
Brayan Markov <145554659+bscript07@users.noreply.github.com>
2025-01-25 18:24:07 +0200

    Update 06_ERC20.js

--- Hardhat-and-Blockchain-Interaction-Lab/tasks/06_ERC20.js
+++ Hardhat-and-Blockchain-Interaction-Lab/tasks/06_ERC20.js
@@ -1,39 +1,32 @@
-const { task } = require("hardhat/config");
+task("ERC20", "Retrieve and display ERC20 token").setAction(async (_, hre) => {
+  const [owner, player1, player2] = await hre.ethers.getSigners();
+  console.log(`Owner address: ${owner.address}`);
+  console.log(`Player 1 address: ${player1.address}`);
+  console.log(`Player 2 address: ${player2.address}`);
 
-task("ERC20", "Retrieve and display ERC20 token")
-.setAction(async(_, hre) => {
-    const {ethers} = hre;
+  const ERC20 = await hre.ethers.getContractFactory("Dogecoin");
+  const token = await ERC20.deploy(
+    "Dogecoin",
+    "DC",
+    hre.ethers.parseUnits("1000000", 18)
+  );
 
-    const [owner, player1, player2] = await ethers.getSigners();
-    console.log(`Owner address: ${owner.address}`);
-    console.log(`Player 1 address: ${player1.address}`);
-    console.log(`Player 2 address: ${player2.address}`);
-    
-    const ERC20 = await ethers.getContractFactory("Dogecoin");
-    console.log(`ERC20 contract: ${ERC20}`);
+  const balancePlayer1Initial = await token.balanceOf(player1.address);
+  const balancePlayer2Initial = await token.balanceOf(player2.address);
+  console.log(`Player 1 initial balance: ${balancePlayer1Initial.toString()}`);
+  console.log(`Player 2 initial balance: ${balancePlayer2Initial.toString()}`);
 
-    const token = await ERC20.deploy("Dogecoin", "DC", ethers.utils.parseUnits("1000000", 18)); // Create initial supply 
+  // Mint 1000 tokens to each player
+  const mintAmount = hre.ethers.parseUnits("1000", 18);
 
-    await token.deployed();
-    console.log("Token deployed to:", token.address);
+  await token.mint(player1.address, mintAmount);
+  await token.mint(player2.address, mintAmount);
 
-    const balancePlayer1Initial = await token.balanceOf(player1.address);
-    const balancePlayer2Initial = await token.balanceOf(player2.address);
-    console.log(`Player 1 initial balance: ${balancePlayer1Initial.toString()}`);
-    console.log(`Player 2 initial balance: ${balancePlayer2Initial.toString()}`);
-
-    // Mint 1000 tokens to each player
-    const mintAmount = ethers.utils.parseUnits("1000", 18);
-
-    await token.mint(player1.address, mintAmount);
-    await token.mint(player2.address, mintAmount);
-
-    // Updated balance
-    const balancePlayer1 = await token.balanceOf(player1.address);
-    const balancePlayer2 = await token.balanceOf(player2.address);
-    console.log(`Player 1 balance: ${balancePlayer1.toString()}`);
-    console.log(`Player 2 balance: ${balancePlayer2.toString()}`);
-
+  // Updated balance
+  const balancePlayer1 = await token.balanceOf(player1.address);
+  const balancePlayer2 = await token.balanceOf(player2.address);
+  console.log(`Player 1 balance: ${balancePlayer1.toString()}`);
+  console.log(`Player 2 balance: ${balancePlayer2.toString()}`);
 });
 
 module.exports = {};
