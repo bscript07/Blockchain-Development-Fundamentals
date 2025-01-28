c39a7224c2b552091998dd41835145c6053f090d
Brayan Markov <145554659+bscript07@users.noreply.github.com>
2025-01-28 19:48:45 +0200

    Update 01_DeployContract.js

--- Hardhat-and-Blockchain-Interaction-Homework/tasks/01_DeployContract.js
+++ Hardhat-and-Blockchain-Interaction-Homework/tasks/01_DeployContract.js
@@ -1,7 +1,6 @@
 task("deployContract", "Deploy EcoBalance contract").setAction(
   async (taskArgs, hre) => {
     const [deployer] = await hre.ethers.getSigners();
-    console.log(`Deploying contract with the account: ${deployer.address}`);
 
     const EcoBalance = await hre.ethers.getContractFactory("EcoBalance");
     const initialSupply = hre.ethers.parseEther("1000000000000000000"); // 1e18 => ``1 ECOB = 1000000000000000000 eco satoshies``
