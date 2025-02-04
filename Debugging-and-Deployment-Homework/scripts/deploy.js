a3e5dc917847cfdf5a888c88f3b6794e4e577e99
Brayan Markov <145554659+bscript07@users.noreply.github.com>
2025-02-04 06:56:09 +0200

    Update deploy.js

--- Debugging-and-Deployment-Homework/scripts/deploy.js
+++ Debugging-and-Deployment-Homework/scripts/deploy.js
@@ -1,6 +1,5 @@
 async function main() {
   const [deployer] = await ethers.getSigners();
-  console.log(`Deployer address: ${deployer.address}`);
 
   const depositTokenAddress = "0xd6f2E74021e7447D70e85734CA2B808d8d9C2027";
   const RewardPool = await ethers.getContractFactory("RewardPoolC");
